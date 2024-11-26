89b4377801dbc71be25341b6671187a7cccd1581
Yun <y5624711@gmail.com>
2024-11-26 16:27:12 +0900

    보드메인페이지&회원가입페이지

---
+++ backend/src/main/java/com/example/backend/controller/board/BoardController.java
@@ -0,0 +1,11 @@
+package com.example.backend.controller.board;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/board")
+@RequiredArgsConstructor
+public class BoardController {
+}
--- backend/src/main/java/com/example/backend/controller/board/MemberController.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.controller.board;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class MemberController {
-}
