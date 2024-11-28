b6d377a761a06d4c7ffc0ef088ab9ac63a6a03e4
Yun <y5624711@gmail.com>
2024-11-28 18:20:33 +0900

    메인페이지 태그 슬라이드 적용

--- frontend/src/page/board/BoardMain.jsx
+++ frontend/src/page/board/BoardMain.jsx
@@ -1,5 +1,30 @@
 import { Box, Center, Flex, Image, Text } from "@chakra-ui/react";
 import { MyHeading } from "../../components/root/MyHeading.jsx";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+function NexArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", backgroundColor: "green" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
 
 export function BoardMain() {
   const categories = [
@@ -10,6 +35,17 @@ export function BoardMain() {
     { name: "스포츠용품", src: "src/components/Image/스포츠용품.jpg" },
     { name: "도서", src: "src/components/Image/도서.jpg" },
   ];
+
+  const sliderSettings = {
+    arrows: true,
+    dots: true,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    nextArrow: <NexArrow />,
+    prevArrow: <PrevArrow />,
+  };
 
   const boxStyle = {
     w: "150px",
@@ -49,6 +85,30 @@ export function BoardMain() {
             </Box>
           ))}
         </Flex>
+      </Center>
+
+      <Center>
+        <Box w="40%">
+          <Slider {...sliderSettings}>
+            {categories.map((category) => (
+              <Box
+                key={category.name}
+                textAlign="center"
+                cursor="pointer"
+                onClick={() => handleClick(category.name)}
+              >
+                <Image
+                  mx="auto"
+                  w="100px"
+                  h="100px"
+                  src={category.src}
+                  alt={category.name}
+                />
+                <Text mt="4">{category.name}</Text>
+              </Box>
+            ))}
+          </Slider>
+        </Box>
       </Center>
     </Box>
   );
