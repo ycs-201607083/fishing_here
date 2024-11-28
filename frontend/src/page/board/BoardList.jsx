acb354b856d588d51f38c3bb41a8693fa067cc2e
June <junetiger10@naver.com>
2024-11-28 20:28:14 +0900

    게시판 데이터 로드 중 Spinner 표시

--- frontend/src/page/board/BoardList.jsx
+++ frontend/src/page/board/BoardList.jsx
@@ -1,42 +1,62 @@
-import { Box, Table } from "@chakra-ui/react";
+import { Box, Center, HStack, Spinner, Table } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 export function BoardList() {
+  // 게시판 데이터 상태
   const [boardList, setBoardList] = useState([]);
+  // 로딩 상태
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    axios
-      .get("/api/board/list")
-      .then((res) => res.data)
-      .then((data) => setBoardList(data));
+    fetchBoardList();
   }, []);
+
+  const fetchBoardList = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get("/api/board/list");
+      setBoardList(response.data);
+    } catch (error) {
+      console.error("데이터를 불러오는 데 실페하였습니다.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Box>
       <h3>게시물 목록</h3>
-      <Table.Root interactive>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeader>번호</Table.ColumnHeader>
-            <Table.ColumnHeader>제목</Table.ColumnHeader>
-            <Table.ColumnHeader>작성자</Table.ColumnHeader>
-            <Table.ColumnHeader>조회수</Table.ColumnHeader>
-            <Table.ColumnHeader>작성일시</Table.ColumnHeader>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {boardList.map((board) => (
-            <Table.Row key={board.number}>
-              <Table.Cell>{board.number}</Table.Cell>
-              <Table.Cell>{board.title}</Table.Cell>
-              <Table.Cell>{board.writer}</Table.Cell>
-              <Table.Cell>{board.viewCount}</Table.Cell>
-              <Table.Cell>{board.date}</Table.Cell>
+      {isLoading ? (
+        <Center h="100vh">
+          <HStack gap="5">
+            <Spinner size="xl" />
+          </HStack>
+        </Center>
+      ) : (
+        <Table.Root interactive>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeader>번호</Table.ColumnHeader>
+              <Table.ColumnHeader>제목</Table.ColumnHeader>
+              <Table.ColumnHeader>작성자</Table.ColumnHeader>
+              <Table.ColumnHeader>조회수</Table.ColumnHeader>
+              <Table.ColumnHeader>작성일시</Table.ColumnHeader>
             </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+          </Table.Header>
+          <Table.Body>
+            {boardList.map((board) => (
+              <Table.Row key={board.number}>
+                <Table.Cell>{board.number}</Table.Cell>
+                <Table.Cell>{board.title}</Table.Cell>
+                <Table.Cell>{board.writer}</Table.Cell>
+                <Table.Cell>{board.viewCount}</Table.Cell>
+                <Table.Cell>{board.date}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      )}
     </Box>
   );
 }
