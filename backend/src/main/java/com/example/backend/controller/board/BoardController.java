eb4fea79a9ffd58c595d56fbf865b67ec2961987
Yun <y5624711@gmail.com>
2024-11-28 17:17:23 +0900

    메인페이지 태그 이미지 추가

--- backend/src/main/java/com/example/backend/controller/board/BoardController.java
+++ backend/src/main/java/com/example/backend/controller/board/BoardController.java
@@ -1,17 +1,24 @@
 package com.example.backend.controller.board;
 
+import com.example.backend.service.board.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/board")
 @RequiredArgsConstructor
 public class BoardController {
 
-    @GetMapping("boardMain")
-    public void boardMain() {
+    final BoardService service;
 
+    @GetMapping("boardMain")
+    public List<String> boardMain() {
+
+        return null;
     }
+
 }
--- frontend/src/page/board/BoardMain.jsx
+++ frontend/src/page/board/BoardMain.jsx
@@ -1,5 +1,67 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Center, Flex, Image, Text } from "@chakra-ui/react";
+import { MyHeading } from "../../components/root/MyHeading.jsx";
+import { useState } from "react";
 
 export function BoardMain() {
-  return <Box>메인페이지</Box>;
+  const [tagList, setTagList] = useState([]);
+
+  function handleClick() {
+    console.log("클릭");
+  }
+
+  const boxStyle = {
+    w: "150px",
+    h: "150px",
+    bgColor: "white",
+    textAlign: "center",
+  };
+
+  const tagImageStyle = {
+    mx: "auto",
+    w: "100px",
+    h: "100px",
+  };
+
+  return (
+    <Box>
+      <MyHeading>메인페이지</MyHeading>
+      <Center mx={"auto"}>
+        <Flex gap="4">
+          <Box {...boxStyle} onClick={handleClick}>
+            <Image
+              {...tagImageStyle}
+              src={"src/components/Image/가전제품.jpg"}
+            />
+            <Text mt="4">가전제품</Text>
+          </Box>
+          <Box {...boxStyle} onClick={handleClick}>
+            <Image
+              {...tagImageStyle}
+              src={"src/components/Image/생활용품.jpg"}
+            />
+            <Text mt="4">생활용품</Text>
+          </Box>
+          <Box {...boxStyle} onClick={handleClick}>
+            <Image {...tagImageStyle} src={"src/components/Image/학용품.jpg"} />
+            <Text mt="4">학용품</Text>
+          </Box>
+          <Box {...boxStyle} onClick={handleClick}>
+            <Image {...tagImageStyle} src={"src/components/Image/의류.jpg"} />
+            <Text mt="4">의류</Text>
+          </Box>
+          <Box {...boxStyle} onClick={handleClick}>
+            <Image
+              {...tagImageStyle}
+              src={"src/components/Image/스포츠용품.jpg"}
+            />
+            <Text mt="4">스포츠용품</Text>
+          </Box>
+          <Box {...boxStyle} onClick={handleClick}>
+            <Image {...tagImageStyle} src={"src/components/Image/도서.jpg"} />
+            <Text mt="4">도서</Text>
+          </Box>
+        </Flex>
+      </Center>
+    </Box>
+  );
 }
