e4e93fa45c21ae5bcf9ee9efa7e6bb7b89bb9371
ycs-201607083 <nan0805@naver.com>
2024-11-29 16:12:03 +0900

    에러 커밋

--- frontend/src/page/manager/MemberList.jsx
+++ frontend/src/page/manager/MemberList.jsx
@@ -1,4 +1,4 @@
-import { Box, Table, TableRoot } from "@chakra-ui/react";
+import { Box, Flex, Table, TableRoot } from "@chakra-ui/react";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -21,35 +21,117 @@ export function MemberList() {
     return <h3>회원 목록이 존재하지 않습니다.</h3>;
   }
   return (
-    <Box
-      mx={"auto"}
-      w={{
-        md: "500px",
-      }}
-    >
-      <MyHeading>회원 목록</MyHeading>
+    
+    <Flex>
+      
+  
+      <Flex
+        wrap="wrap" // 공간이 부족할 때 다음 줄로 넘어감
+        gap="10%" // 박스 간 간격
+        p="4" // 패딩
+        bg="gray.100"
+        w={"100%"}
+        h={"100vh"}
+      >
 
-      <TableRoot interactive>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeader>ID</Table.ColumnHeader>
-            <Table.ColumnHeader>EMAIL</Table.ColumnHeader>
-            <Table.ColumnHeader>가입일시</Table.ColumnHeader>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {memberList.map((member) => (
-            <Table.Row
-              omClick={() => handleRowClick(member.id)}
-              key={member.id}
-            >
-              <Table.Cell>{member.id}</Table.Cell>
-              <Table.Cell>{member.email}</Table.Cell>
-              <Table.Cell>{member.inserted}</Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </TableRoot>
-    </Box>
+        <MyHeading>회원 목록</MyHeading>
+
+        <Box
+          w="50%"
+          h="100%"
+          overflow="auto" // 스크롤 활성화
+          border="1px solid"
+          borderColor="gray.300"
+          p="4"
+        >
+          <TableRoot interactive>
+            <Table.Header>
+              <Table.Row>
+                <Table.ColumnHeader>ID</Table.ColumnHeader>
+                <Table.ColumnHeader>EMAIL</Table.ColumnHeader>
+                <Table.ColumnHeader>가입일시</Table.ColumnHeader>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {memberList.map((member) => (
+                <Table.Row
+                  omClick={() => handleRowClick(member.id)}
+                  key={member.id}
+                >
+                  <Table.Cell>{member.id}</Table.Cell>
+                  <Table.Cell>{member.email}</Table.Cell>
+                  <Table.Cell>{member.inserted}</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </TableRoot>
+        </Box>
+        <Box
+          w="300px"
+          h="200px"
+          overflow="auto" // 스크롤 활성화
+          border="1px solid"
+          borderColor="gray.300"
+          p="4"
+        >
+          <MyHeading>회원 목록</MyHeading>
+
+          <TableRoot interactive>
+            <Table.Header>
+              <Table.Row>
+                <Table.ColumnHeader>ID</Table.ColumnHeader>
+                <Table.ColumnHeader>EMAIL</Table.ColumnHeader>
+                <Table.ColumnHeader>가입일시</Table.ColumnHeader>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {memberList.map((member) => (
+                <Table.Row
+                  omClick={() => handleRowClick(member.id)}
+                  key={member.id}
+                >
+                  <Table.Cell>{member.id}</Table.Cell>
+                  <Table.Cell>{member.email}</Table.Cell>
+                  <Table.Cell>{member.inserted}</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </TableRoot>
+        </Box>
+        <Box
+          w="300px"
+          h="200px"
+          overflow="auto" // 스크롤 활성화
+          border="1px solid"
+          borderColor="gray.300"
+          p="4"
+        >
+          <MyHeading>회원 목록</MyHeading>
+
+          <TableRoot interactive>
+            <Table.Header>
+              <Table.Row>
+                <Table.ColumnHeader>ID</Table.ColumnHeader>
+                <Table.ColumnHeader>EMAIL</Table.ColumnHeader>
+                <Table.ColumnHeader>가입일시</Table.ColumnHeader>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {memberList.map((member) => (
+                <Table.Row
+                  omClick={() => handleRowClick(member.id)}
+                  key={member.id}
+                >
+                  <Table.Cell>{member.id}</Table.Cell>
+                  <Table.Cell>{member.email}</Table.Cell>
+                  <Table.Cell>{member.inserted}</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </TableRoot>
+        </Box>
+      </Flex>
+    </Flex>
+
   );
 }
