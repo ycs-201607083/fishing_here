55ede9accaecb525f75435a8dee7eb3d5c995559
Yun <y5624711@gmail.com>
2024-12-03 14:21:49 +0900

    맵 api 작업

--- frontend/src/page/board/BoardMap.jsx
+++ frontend/src/page/board/BoardMap.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Box} from "@chakra-ui/react";
+import GoogleMapApi from "../../components/map/GoogleMapApi.jsx";
 
 export function BoardMap(props) {
   return (
     <Box>
-
+      <GoogleMapApi/>
     </Box>
   );
 }
