b7262ae5e49de88c079711f1f834a489b068e2c8
Yun <y5624711@gmail.com>
2024-12-04 10:32:20 +0900

    병합작업 완료

--- backend/src/main/java/com/example/backend/dto/board/Board.java
+++ backend/src/main/java/com/example/backend/dto/board/Board.java
@@ -7,11 +7,10 @@ import java.time.LocalDateTime;
 @Data
 public class Board {
     private Integer number;
-    private Integer tagNumber;
     private String title;
     private String content;
     private String writer;
     private Integer viewCount;
     private LocalDateTime date;
-    private String site;
+    private String site;//민물낚시, 바다낚시
 }
