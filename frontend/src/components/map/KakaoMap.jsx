f605190e05ee49c27a759c0070e04a3b3b0c2e2a
Yun <y5624711@gmail.com>
2024-12-04 10:42:29 +0900

    병합작업 완료

--- frontend/src/components/map/KakaoMap.jsx
+++ frontend/src/components/map/KakaoMap.jsx
@@ -1,22 +1,22 @@
-import {Box, Group, HStack, IconButton, Input, Stack} from "@chakra-ui/react";
-import {useEffect, useState} from "react";
-import {Field} from "../ui/field.jsx";
-import {LuSearch} from "react-icons/lu";
+import { Box, Group, HStack, IconButton, Input, Stack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Field } from "../ui/field.jsx";
+import { LuSearch } from "react-icons/lu";
 
-const {kakao} = window;
+const { kakao } = window;
 
 export function KakaoMap() {
   const [map, setMap] = useState(null);
-  const [keyWord, setKeyWord] = useState("");//검색어
-  const [searchService, setSearchService] = useState(null);//장소검색서비스
-  const [marker, setMarker] = useState([]);//마커 관리
+  const [keyWord, setKeyWord] = useState(""); //검색어
+  const [searchService, setSearchService] = useState(null); //장소검색서비스
+  const [marker, setMarker] = useState([]); //마커 관리
 
   useEffect(() => {
     //지도 초기화
-    const container = document.getElementById('map');
+    const container = document.getElementById("map");
     const options = {
       center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3
+      level: 3,
     };
     const mapInstance = new kakao.maps.Map(container, options);
     //장소검색 초기화
@@ -32,14 +32,12 @@ export function KakaoMap() {
     setMarker([]);
   }
 
-
   const handleClickButton = () => {
     if (!keyWord || !searchService || !map) {
       return;
     }
 
     clearMarker();
-
 
     searchService.keywordSearch(keyWord, (data, status) => {
       if (status === kakao.maps.services.Status.OK) {
@@ -62,9 +60,9 @@ export function KakaoMap() {
         //지도범위 확장
         map.setBounds(bounds);
       } else {
-        alert("검색결과가 없습니다.")
+        alert("검색결과가 없습니다.");
       }
-    })
+    });
   };
 
   const handleEnterKey = (e) => {
@@ -73,40 +71,57 @@ export function KakaoMap() {
     }
   };
 
-  return <HStack
-    gap={"5"}
-    mx="auto"
-    w={{md: "1500px"}}
-    mt="100px"
-    bgColor="gray.100"
-    borderRadius="md"
-    boxShadow="md"
-    spacing={4}
-    p={4}>
-    <Box id="map"
-         w="1000px"
-         h="600px"
-         bgColor="white"
-         borderRadius="md"
-         boxShadow="md"></Box>
-    <Stack w="500px" h="600px" mx="auto" p={4} borderRadius="md" bgColor="blue.300">
-      <Field>
-        <Group mx="auto">
-          <Input variant="subtle"
-                 type="text"
-                 placeholder="검색어를 입력하세요"
-                 w="300px"
-                 h="38px"
-                 value={keyWord} onChange={(e) => setKeyWord(e.target.value)}
-                 onKeyDown={handleEnterKey}/>
-          <IconButton aria-label="Search database" onClick={handleClickButton}>
-            <LuSearch/>
-          </IconButton>
-        </Group>
-      </Field>
-      <Box bgColor="white" w="450px" h="600px" mx="auto"></Box>
-    </Stack>
-    <Box>
-    </Box>
-  </HStack>
+  return (
+    <HStack
+      gap={"5"}
+      mx="auto"
+      w={{ md: "1500px" }}
+      mt="100px"
+      bgColor="gray.100"
+      borderRadius="md"
+      boxShadow="md"
+      spacing={4}
+      p={4}
+    >
+      <Box
+        id="map"
+        w="1000px"
+        h="600px"
+        bgColor="white"
+        borderRadius="md"
+        boxShadow="md"
+      ></Box>
+      <Stack
+        w="500px"
+        h="600px"
+        mx="auto"
+        p={4}
+        borderRadius="md"
+        bgColor="blue.300"
+      >
+        <Field>
+          <Group mx="auto">
+            <Input
+              variant="subtle"
+              type="text"
+              placeholder="검색어를 입력하세요"
+              w="300px"
+              h="38px"
+              value={keyWord}
+              onChange={(e) => setKeyWord(e.target.value)}
+              onKeyDown={handleEnterKey}
+            />
+            <IconButton
+              aria-label="Search database"
+              onClick={handleClickButton}
+            >
+              <LuSearch />
+            </IconButton>
+          </Group>
+        </Field>
+        <Box bgColor="white" w="450px" h="600px" mx="auto"></Box>
+      </Stack>
+      <Box></Box>
+    </HStack>
+  );
 }
