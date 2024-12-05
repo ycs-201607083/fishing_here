b181bb42973dc9ca6aadf4cb206e82a2d09a1e16
Yun <y5624711@gmail.com>
2024-12-05 11:25:42 +0900

    nav바 폰트 적용 및 디자인 중

--- frontend/src/components/root/Navbar.jsx
+++ frontend/src/components/root/Navbar.jsx
@@ -1,16 +1,53 @@
 import { useNavigate } from "react-router-dom";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Center, Flex, Spacer } from "@chakra-ui/react";
+
+function NavItem({ children, ...rest }) {
+  return (
+    <Box whiteSpace="nowrap" {...rest}>
+      {children}
+    </Box>
+  );
+}
 
 export function Navbar() {
   const navigate = useNavigate();
 
+  const menuStyle = {};
+
   return (
-    <Flex gap={5}>
-      <Box onClick={() => navigate("/")}>메인</Box>
-      <Box onClick={() => navigate("/board/map")}>지도검색</Box>
-      <Box onClick={() => navigate("/board/list")}>커뮤니티</Box>
-      <Box onClick={() => navigate("/member/signup")}>회원가입</Box>
-      <Box onClick={() => navigate("member/login")}>로그인</Box>
-    </Flex>
+    <Box>
+      <Center
+        color={"blue.800"}
+        fontSize={"40px"}
+        h={"100px"}
+        w={"100%"}
+        onClick={() => navigate("/")}
+        fontFamily={"SANGJU Gotgam"}
+      >
+        자리요
+      </Center>
+      <Flex p={4} justify="center" bgColor={"blue.500"} w="100%">
+        <Spacer />
+        <Flex gap={5}>
+          <NavItem onClick={() => navigate("/board/map")}>낚시터찾기</NavItem>
+          <NavItem {...menuStyle} onClick={() => navigate("/board/list")}>
+            커뮤니티
+          </NavItem>
+        </Flex>
+        <Spacer />
+        <Flex gap={5}>
+          <NavItem {...menuStyle} hideBelow={"sm"}>
+            회원관리
+          </NavItem>
+          <NavItem {...menuStyle} onClick={() => navigate("/member/signup")}>
+            회원가입
+          </NavItem>
+          <NavItem {...menuStyle} onClick={() => navigate("member/login")}>
+            로그인
+          </NavItem>
+          <NavItem {...menuStyle}>로그아웃</NavItem>
+        </Flex>
+      </Flex>
+    </Box>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
 import { Provider } from "./components/ui/provider.jsx";
+import "./font/font.css";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
