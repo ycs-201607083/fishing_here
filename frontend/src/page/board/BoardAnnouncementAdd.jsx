15483fe5cb169acd1fc7faa88cee0c626e40341e
Yun <y5624711@gmail.com>
2024-12-09 17:16:44 +0900

    공지사항페이지

--- frontend/src/page/board/BoardAnnouncementAdd.jsx
+++ frontend/src/page/board/BoardAnnouncementAdd.jsx
@@ -1,21 +1,19 @@
-import React from "react";
-import { Box, Input, Stack } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, Input } from "@chakra-ui/react";
+
 import { Field } from "../../components/ui/field.jsx";
 
 export function BoardAnnouncementAdd() {
+  const [content, setContent] = useState("");
+
   return (
     <Box>
-      <Stack>
-        <Field label={"제목"}>
-          <Input />
-        </Field>
-        <Field label={"본문"}>
-          <Input />
-        </Field>
-        <Field>
-          <Input />
-        </Field>
-      </Stack>
+      <Field label="제목">
+        <Input placeholder="제목을 입력해 주세요" />
+      </Field>
+      <Box>
+        <ReactQuill></ReactQuill>
+      </Box>
     </Box>
   );
 }
