cb6f7c69a0c0f8aa5203263d3873748e2fc7c2b8
June <junetiger10@naver.com>
2024-12-18 21:07:32 +0900

    댓글에 어획량 차트 추가

--- frontend/src/components/comment/CommentContainer.jsx
+++ frontend/src/components/comment/CommentContainer.jsx
@@ -3,6 +3,10 @@ import { CommentInput } from "./CommentInput.jsx";
 import { CommentList } from "./CommentList.jsx";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { ArcElement, Chart as ChartJS, Legend, Tooltip } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+/*차트 요소 등록 코드*/
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export function CommentContainer({ boardId }) {
   const [commentList, setCommentList] = useState([]);
@@ -57,8 +61,29 @@ export function CommentContainer({ boardId }) {
       });
   }
 
+  /*차트 데이터*/
+  const data = {
+    labels: ["Red", "Blue", "Yellow"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [300, 50, 100],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
+
   return (
     <Box>
+      <Box w="400px" h="400px">
+        {/* Chart.js 컴포넌트 렌더링 */}
+        <Doughnut data={data} />
+      </Box>
       <Stack gap={5}>
         <h3>댓글</h3>
         <CommentInput boardId={boardId} onSaveClick={handleSaveClick} />
